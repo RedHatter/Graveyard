1f428783dbf9db9e7124e6f63b4fb8bf6d9b7e6d
Arakys7 <Arakys7@users.noreply.github.com>
2017-08-24 21:05:29 -0700

    Forgotten files

---
+++ src/DKGuldanView.cs
@@ -0,0 +1,24 @@
+using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Hearthstone;
+
+namespace HDT.Plugins.Graveyard
+{
+	public class DKGuldanView : NormalView
+	{
+		public static bool isValid()
+		{
+			return Core.Game.Player.PlayerCardList.FindIndex(card => card.Id == HearthDb.CardIds.Collectible.Warlock.BloodreaverGuldan) > -1;
+		}
+
+		public DKGuldanView()
+		{
+			// Section Label
+			Label.Text = "Gul'dan";
+		}
+
+		new public bool Update(Card card)
+		{
+			return card.Race.Contains("Demon") && base.Update(card);
+		}
+	}
+}
