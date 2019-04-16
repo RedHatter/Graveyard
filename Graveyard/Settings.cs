fa3fae4801611d7b6782ce713e4a489751a06677
Batian Styx <batstyx@gmail.com>
2019-04-16 17:19:34 +0100

    Store settings in Hearthstone Deck Tracker config directory

---
+++ Graveyard/Settings.cs
@@ -0,0 +1,78 @@
+﻿using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HDT.Plugins.Graveyard
+{
+    public class Setting
+    {
+        public Setting() { }
+        public Setting(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public sealed partial class Settings
+    {
+        private const string Filename = "Graveyard.xml";
+        internal static string DataDir => Config.Instance.DataDir;
+        private static string SettingsPath => Path.Combine(DataDir, Filename);
+
+        public Settings()
+        {
+            var provider = Providers;
+
+            SettingsLoaded += SettingsLoadedEventHandler;
+            SettingsSaving += SettingsSavingEventHandler;
+        }
+
+        private void SettingsLoadedEventHandler(object sender, System.Configuration.SettingsLoadedEventArgs e)
+        {
+            try
+            {
+                if (File.Exists(SettingsPath))
+                {
+                    var actual = XmlManager<List<Setting>>.Load(SettingsPath);
+
+                    foreach (var setting in actual)
+                    {
+                        this[setting.Name] = Convert.ChangeType(setting.Value, this.Properties[setting.Name].PropertyType);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
+
+        private void SettingsSavingEventHandler(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                var saveFormat = PropertyValues.Cast<SettingsPropertyValue>()
+                    .Where(p => p.SerializedValue.ToString() != p.Property.DefaultValue.ToString())
+                    .Select(p => new Setting(p.Name, p.SerializedValue.ToString()))
+                    .ToList();
+
+                XmlManager<List<Setting>>.Save(SettingsPath, saveFormat);
+
+                e.Cancel = true;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+            }
+        }
+    }
+}
