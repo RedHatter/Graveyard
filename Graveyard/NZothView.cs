3b365e30a47cda207650734ab106a69fba492389
Batian Styx <batstyx@gmail.com>
2019-11-17 11:09:45 +0000

    Support for Doom in the Tomb re-introduction of NZoth

--- Graveyard/NZothView.cs
+++ Graveyard/NZothView.cs
@@ -9,7 +9,8 @@ namespace HDT.Plugins.Graveyard
 	{
 		public static bool isValid()
 		{
-			return Core.Game.Player.PlayerCardList.FindIndex(card => card.Id == HearthDb.CardIds.Collectible.Neutral.NzothTheCorruptor) > -1;
+			return Core.Game.Player.PlayerCardList.FindIndex(card => card.Id == HearthDb.CardIds.Collectible.Neutral.NzothTheCorruptorOG 
+            || card.Id == HearthDb.CardIds.Collectible.Neutral.NzothTheCorruptorWILD_EVENT) > -1;
 		}
 
 		public NZothView()
