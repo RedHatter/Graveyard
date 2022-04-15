c52d0c009d40bc8123b25497c809dde595514d2c
Batian Styx <batstyx@gmail.com>
2022-04-15 15:23:20 +0100

    Remove Anyfin damage calculator TODO: Update Anyfin Calculator standalone plugin

--- Graveyard/AnyfinView.cs
+++ Graveyard/AnyfinView.cs
@@ -19,7 +19,7 @@ namespace HDT.Plugins.Graveyard
 				Enabled = "AnyfinEnabled",
 				UpdateOn = GameEvents.OnPlayerPlayToGraveyard,
 				CreateView = () => new AnyfinView(),
-				Condition = card => card.IsMurloc(),
+				Condition = card => card.Race == "Murloc" || card.Race == "All",
 			});
 		}				
 
@@ -36,50 +36,5 @@ namespace HDT.Plugins.Graveyard
 				RegisterForCardEvent(GameEvents.OnOpponentPlayToGraveyard, view.Update, isDefault);
 			}
         }
-
-		private HearthstoneTextBlock _dmg;
-		private HearthstoneTextBlock _secondDmg;
-
-		public AnyfinView()
-		{
-			// Damage Label
-			_dmg = new HearthstoneTextBlock();
-			_dmg.FontSize = 24;
-			_dmg.TextAlignment = TextAlignment.Center;
-			_dmg.Text = "0";
-			Children.Add(_dmg);
-			_dmg.Visibility = Visibility.Hidden;
-
-			_secondDmg = new HearthstoneTextBlock();
-			_secondDmg.FontSize = 24;
-			_secondDmg.TextAlignment = TextAlignment.Center;
-			_secondDmg.Text = "0";
-			Children.Add(_secondDmg);
-			_secondDmg.Visibility = Visibility.Hidden;
-		}
-
-		public override bool Update(Card card)
-		{
-			if (base.Update(card))
-			{
-				UpdateDamage();
-				return true; 
-			}
-			return false;
-		}
-
-		public void UpdateDamage()
-		{
-			// Update damage counter
-			Range<int> damage = AnyfinCalculator.CalculateDamageDealt(Cards);
-			_dmg.Text = damage.Minimum == damage.Maximum ? damage.Maximum.ToString() : $"{damage.Minimum} - {damage.Maximum}";
-			_dmg.Visibility = Cards.Count > 0 ? Visibility.Visible : Visibility.Hidden;
-
-			List<Card> moreCards = Cards.Select(c => c.Clone() as Card).ToList();
-			moreCards.AddRange(Cards);
-			Range<int> secondDamage = AnyfinCalculator.CalculateDamageDealt(moreCards);
-			_secondDmg.Text = damage.Minimum == damage.Maximum ? secondDamage.Maximum.ToString() : $"{secondDamage.Minimum} - {secondDamage.Maximum}";
-			_secondDmg.Visibility = Cards.Count > 0 ? Visibility.Visible : Visibility.Hidden;
-		}
 	}
 }
