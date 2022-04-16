afc11f97bff5c7d32db235b1856e4bad44e400ee
Batian Styx <batstyx@gmail.com>
2022-04-16 09:37:13 +0100

    Strings returns key if no localized value Instead of null

--- Graveyard/Strings.cs
+++ Graveyard/Strings.cs
@@ -4,6 +4,6 @@ namespace HDT.Plugins.Graveyard
 {
     public class Strings
     {
-        public static string GetLocalized(string key) => LocalizeDictionary.Instance.GetLocalizedObject("Graveyard", "Strings", key, LocalizeDictionary.Instance.Culture)?.ToString();
+        public static string GetLocalized(string key) => LocalizeDictionary.Instance.GetLocalizedObject("Graveyard", "Strings", key, LocalizeDictionary.Instance.Culture)?.ToString() ?? key;
     }
 }
