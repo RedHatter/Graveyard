557c73514dd6edbc2c52baf564f0e3844590c4ac
Batian Styx <batstyx@gmail.com>
2022-08-31 20:25:21 +0100

    Add Kel'Thuzad the Inevitable view

---
+++ Graveyard/KelthuzadTheInevitableView.cs
@@ -0,0 +1,21 @@
+﻿using Hearthstone_Deck_Tracker.API;
+using static HearthDb.CardIds.Collectible;
+
+namespace HDT.Plugins.Graveyard
+{
+    public class KelthuzadTheInevitableView
+    {
+        private static ViewConfig _Config;
+        internal static ViewConfig Config
+        {
+            get => _Config ?? (_Config = new ViewConfig(Mage.KelthuzadTheInevitable)
+            {
+                Name = "KelthuzadTheInevitable",
+                ShowFirst = () => true,
+                CreateView = () => new NormalView(),
+                UpdateOn = GameEvents.OnPlayerPlayToGraveyard,
+                Condition = card => card.Id == Neutral.VolatileSkeleton,
+            });
+        }
+    }
+}
