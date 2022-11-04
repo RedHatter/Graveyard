e65118fe837ef31b226226603cbfa1eb709bef94
Batian Styx <batstyx@gmail.com>
2022-11-04 08:25:31 +0000

    Add Abominable Bowman to Witching Hour view

--- Graveyard/WitchingHourView.cs
+++ Graveyard/WitchingHourView.cs
@@ -8,7 +8,10 @@ namespace HDT.Plugins.Graveyard
         private static ViewConfig _Config;
         internal static ViewConfig Config
         {
-            get => _Config ?? (_Config = new ViewConfig(Druid.WitchingHour, Hunter.RevivePet)
+            get => _Config ?? (_Config = new ViewConfig(
+                Druid.WitchingHour, 
+                Hunter.RevivePet, 
+                Hunter.AbominableBowmanCore)
             {
                 Name = "WitchingHour",
                 Enabled = "WitchingHourEnabled",
