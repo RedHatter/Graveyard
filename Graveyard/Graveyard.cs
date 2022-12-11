308b0cfd705052da8263fb6790288ae5a4653fe5
Batian Styx <batstyx@gmail.com>
2022-12-11 19:45:40 +0000

    - Separate Unending Swarm view - Include AnimateDeadView in project - Change AnimateDeadView scope to public

--- Graveyard/Graveyard.cs
+++ Graveyard/Graveyard.cs
@@ -57,6 +57,7 @@ namespace HDT.Plugins.Graveyard
 			ReanimateView.Config,
 			HighCultistBasalephView.Config,
 			AnimateDeadView.Config,
+			UnendingSwarmView.Config,
 		};
 
 		private readonly StackPanel FriendlyPanel;
