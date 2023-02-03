53e2ea7f13bd1573a06a956b23f90b15127feb06
Batian Styx <batstyx@gmail.com>
2023-02-03 10:29:14 +0000

    Use HoldingEffectView for Hedra (So it actually works)

--- Graveyard/HedraView.cs
+++ Graveyard/HedraView.cs
@@ -1,54 +1,18 @@
-﻿using Hearthstone_Deck_Tracker.API;
-using Hearthstone_Deck_Tracker.Hearthstone;
+﻿using static HDT.Plugins.Graveyard.HoldingEffectView;
 using static HearthDb.CardIds.Collectible;
 
 namespace HDT.Plugins.Graveyard
 {
-    public class HedraView : NormalView
+	internal class HedraView
     {
 		private static ViewConfig _Config;
 		internal static ViewConfig Config
 		{
-			get => _Config ?? (_Config = new HedraViewConfig()
+			get => _Config ?? (_Config = new HoldingEffectConfig(Druid.HedraTheHeretic)
 			{
-				Name = "Hedra",
-				CreateView = () => new HedraView(),
-				UpdateOn = GameEvents.OnPlayerPlay,
+				Name = "Hedra",				
 				Condition = card => card.Type == "Spell",
 			});
-		}
-
-		private static bool IsHedra(Card card) => card.Id == Druid.HedraTheHeretic;
-		private bool HedraInHand = false;
-
-		public override bool Update(Card card)
-        {
-			if (HedraInHand && IsHedra(card)) HedraInHand = false;
-			return HedraInHand && base.Update(card);
-        }
-
-		public bool PlayerPlayToHand(Card card)
-        {
-			if (!HedraInHand && IsHedra(card)) HedraInHand = true;
-			return HedraInHand;
-        }
-
-        internal class HedraViewConfig : ViewConfig
-		{
-			public HedraViewConfig() : base(Druid.HedraTheHeretic)
-			{
-
-			}
-
-			public override void RegisterView(ViewBase view, bool isDefault = false)
-			{
-				base.RegisterView(view, isDefault);
-                if (view is HedraView hedraview)
-                {
-					RegisterForCardEvent(GameEvents.OnPlayerPlayToHand, hedraview.PlayerPlayToHand);
-				}
-				
-			}
 		}
 	}
 }
