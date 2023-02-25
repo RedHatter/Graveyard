0485267c219691fd977a6d284c57677c3392bb97
Batian Styx <batstyx@gmail.com>
2023-02-25 19:54:05 +0000

    Add list of non-eponymous imps Include multi-race imps for now

--- Graveyard/ImpKingView.cs
+++ Graveyard/ImpKingView.cs
@@ -1,9 +1,7 @@
 ﻿using Hearthstone_Deck_Tracker.API;
-using System;
+using Hearthstone_Deck_Tracker.Hearthstone;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static HearthDb.CardIds;
 using static HearthDb.CardIds.Collectible;
 
 namespace HDT.Plugins.Graveyard
@@ -18,8 +16,28 @@ namespace HDT.Plugins.Graveyard
 				Name = "ImpKing",
 				CreateView = () => new ChancesView(),
 				UpdateOn = GameEvents.OnPlayerPlayToGraveyard,
-				Condition = card => card.Race == "Demon" && card.Name.Contains("Imp") && card.Id != Neutral.ImprisonedVilefiend,
+				Condition = card => Imps.Contains(card.Id) || IsEponymousImp(card),
 			});
 		}
-	}
+
+		internal static bool IsEponymousImp(Card card)
+		{
+			return card.Race == "Demon" && card.Name.Contains("Imp") && card.Id != Neutral.ImprisonedVilefiend;
+        }
+		
+        internal static readonly List<string> Imps = new List<string>
+        {
+            NonCollectible.Warlock.DarkAlleyPact_FiendToken,
+            NonCollectible.Neutral.RustswornCultist_RustedDevilToken,
+			NonCollectible.Warlock.NofinsImpossible_ImplocToken, 
+			Warlock.ImprisonedScrapImp,
+            Warlock.TinyKnightOfEvilCore,
+			Warlock.VulgarHomunculus,
+			Neutral.StreetTrickster,
+			Warlock.UnlicensedApothecary,
+			Neutral.SneakyDevil,
+			Warlock.EnvoyRustwix,
+			Warlock.RingMatron,
+        };
+    }
 }
