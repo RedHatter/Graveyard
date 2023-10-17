ee3f6170826847646d6a50e8e868e0552c5219e0
Batian Styx <batstyx@gmail.com>
2023-10-17 09:28:15 +0100

    Fix MissingMethodException error Fixed by switching from ?? to separate if for LocUtil.Get call Cause of error unclear

--- Graveyard/Strings.cs
+++ Graveyard/Strings.cs
@@ -1,5 +1,7 @@
 ﻿using HDT.Plugins.Graveyard.Properties;
 using Hearthstone_Deck_Tracker.Utility;
+using Hearthstone_Deck_Tracker.Utility.Logging;
+using System;
 using WPFLocalizeExtension.Engine;
 
 namespace HDT.Plugins.Graveyard
@@ -8,8 +10,20 @@ namespace HDT.Plugins.Graveyard
     {
         public static string GetLocalized(string key)
         {
-            var value = LocalizeDictionary.Instance.GetLocalizedObject(LibraryInfo.Name, "Strings", key, LocalizeDictionary.Instance.Culture)?.ToString()
-            ?? LocUtil.Get(key);
+            var value = LocalizeDictionary.Instance.GetLocalizedObject(LibraryInfo.Name, "Strings", key, LocalizeDictionary.Instance.Culture)?.ToString();
+
+            if (value == null)
+            {
+                try
+                {
+                    Log.Info($"Local {key} string not available, trying to use HDT string instead");
+                    value = LocUtil.Get(key);
+                }
+                catch (MissingMethodException ex)
+                {
+                    Log.Error(ex);
+                }
+            }
 
             return string.IsNullOrEmpty(value) ? key : value;
         }
