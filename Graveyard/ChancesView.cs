a2631cc5107b9c1f3b14625ac124fcd18f8b949e
Batian Styx <batstyx@gmail.com>
2024-03-15 18:58:38 +0000

    Adjust ChancesView layout to justify with other views Use ChancesViewLeft setting

--- Graveyard/ChancesView.cs
+++ Graveyard/ChancesView.cs
@@ -1,10 +1,24 @@
-﻿using Hearthstone_Deck_Tracker.Hearthstone;
+﻿using Hearthstone_Deck_Tracker;
+using Hearthstone_Deck_Tracker.Hearthstone;
+using System.Windows;
 
 namespace HDT.Plugins.Graveyard
 {
     public class ChancesView : NormalView
     {
         private readonly ChancesTracker Chances = new ChancesTracker();
+
+        public ChancesView() : base()
+        {
+            Margin = new Thickness(Margin.Left + Settings.Default.ChancesViewLeft, Margin.Top, Margin.Right, Margin.Right);
+        }
+
+        public override HearthstoneTextBlock AddTitle(string text = "")
+        {
+            var label = base.AddTitle(text);
+            label.Margin = new Thickness(label.Margin.Left - Settings.Default.ChancesViewLeft, label.Margin.Top, label.Margin.Right, label.Margin.Right);
+            return label;
+        }
 
         public override bool Update(Card card)
         {
