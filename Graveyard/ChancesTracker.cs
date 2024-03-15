31575715f674cf14f0a3c3cba04638096558e3bb
Batian Styx <batstyx@gmail.com>
2024-03-15 18:52:54 +0000

    Update ChancesTracker for changes in AnimatedCard Add warnings if reference types aren't as expected Fixes #33

--- Graveyard/ChancesTracker.cs
+++ Graveyard/ChancesTracker.cs
@@ -7,32 +7,47 @@ using System.Collections.Generic;
 using Hearthstone_Deck_Tracker;
 using Hearthstone_Deck_Tracker.Controls;
 using Card = Hearthstone_Deck_Tracker.Hearthstone.Card;
+using Hearthstone_Deck_Tracker.Utility.Logging;
 
 namespace HDT.Plugins.Graveyard
 {
 	public class ChancesTracker
 	{
-		private Dictionary<Card, HearthstoneTextBlock> _chances = new Dictionary<Card, HearthstoneTextBlock>();
+        private readonly Dictionary<Card, HearthstoneTextBlock> Chances = new Dictionary<Card, HearthstoneTextBlock>();
 
 		public void Update(Card card, List<Card> Cards, AnimatedCardList View)
 		{
 			var count = (double)Cards.Aggregate(0, (total, c) => total + c.Count);
 			for (var i = 0; i < Cards.Count(); i++)
 			{
-				if (!_chances.ContainsKey(Cards[i]))
+				if (!Chances.ContainsKey(Cards[i]))
 				{
-					var chance = new HearthstoneTextBlock();
-					chance.FontSize = 18;
-					chance.TextAlignment = TextAlignment.Left;
-					var grid = (View.Items.GetItemAt(i) as UserControl).Content as Grid;
-					grid.Width = 260;
-					(grid.Children[0] as Hearthstone_Deck_Tracker.Controls.Card).HorizontalAlignment = HorizontalAlignment.Right;
-					(grid.Children[1] as Rectangle).Width = 260;
-					grid.Children.Add(chance);
-					_chances.Add(Cards[i], chance);
+                    var chance = new HearthstoneTextBlock
+                    {
+                        FontSize = 18,
+                        TextAlignment = TextAlignment.Left
+                    };
+					if (View.Items.GetItemAt(i) is UserControl control && control.Content is Grid grid)
+					{
+						grid.Width = 260;
+						if (grid.Children[0] is Hearthstone_Deck_Tracker.Controls.Card cardControl)
+							cardControl.HorizontalAlignment = HorizontalAlignment.Right; 
+						else
+                            Log.Warn("Expected Hearthstone_Deck_Tracker.Controls.Card, check AnimatedCard for layout changes");
+						if (grid.Children[2] is Rectangle rectangle)
+                            rectangle.Width = 260;
+						else
+                            Log.Warn("Expected Rectangle, check AnimatedCard for layout changes");
+                        grid.Children.Add(chance); 
+					}
+					else
+					{
+						Log.Warn("Expected UserControl and Grid, check AnimatedCard for layout changes");
+					}
+					Chances.Add(Cards[i], chance);
 				}
 
-				_chances[Cards[i]].Text = $"{Math.Round(Cards[i].Count / count * 100)}%";
+				Chances[Cards[i]].Text = $"{Math.Round(Cards[i].Count / count * 100)}%";
 			}
 		}
 	}
